3d715e641369b2e7533ebf6cc8e82e6a29f535f7
Nosakeys <silaskeys95@gmail.com>
2023-08-15 15:57:04 +0100

    changeofname

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-[#a18674]"> Hi, my name is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
-          Aigbe Silas
+          Silas Aigbe
         </h1>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
           I'm a Full Stack Developer.
