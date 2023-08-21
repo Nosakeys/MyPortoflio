28af3c264ff7040f0c656f115e35bafea4930c47
Nosakeys <silaskeys95@gmail.com>
2023-08-22 00:30:10 +0100

    btn...

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ import { HiOutlineMail } from "react-icons/hi";
 
 const Footer = () => {
   return (
-    <div className='pb-14 px-4 align-top overflow-hidden'>
-    <div className='sm:hidden h-[20svh] border-y-[0.56px] border-pink-300 flex flex-col items-center justify-center gap-5 align-top overflow-hidden'>
+    <div className='pb-14 px-4 align-top overflow-hidden '>
+    <div className='sm:hidden h-[20vh] border-y-[0.56px] border-pink-300 flex flex-col items-center justify-center gap-5 align-top overflow-hidden'>
       <div>
       <p className="text-gray-300 text-center">© Silas Aigbe, All Rights Reserved</p>
       </div>
@@ -13,16 +13,16 @@ const Footer = () => {
         <ul>
           <li className='flex items-center justify-center gap-5'>
           <a className='text-gray-300' href="https://twitter.com/Nosakeys"  target="_blank" rel="noopener noreferrer">
-            <FaTwitter size={25} /> 
+            <FaTwitter size={23} /> 
             </a>
             <a className='text-gray-300' href="https://github.com/Nosakeys"target="_blank" rel="noopener noreferrer">
-            <FaGithub size={25} />
+            <FaGithub size={23} />
             </a>
             <a className='text-gray-300' href="https://www.linkedin.com/in/silas-aigbe-8b506019b"target="_blank" rel="noopener noreferrer">
-            <FaLinkedin size={24} />
+            <FaLinkedin size={23} />
             </a>
             <a className='text-gray-300'   href="mailto:silaskeys95@gmail.com"target="_blank" rel="noopener noreferrer">
-           <HiOutlineMail size={25} />
+           <HiOutlineMail size={23} />
             </a>
           </li>
         </ul>
